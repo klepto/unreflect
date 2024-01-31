6c75e61d9db7660ff98db1649c146cb99ea189a4
klepto <klepto@inbox.lt>
2024-01-31 10:44:55 +0200

    feat: Implement helper initializers for Reflection API types

--- src/main/java/dev/klepto/unreflect/Unreflect.java
+++ src/main/java/dev/klepto/unreflect/Unreflect.java
@@ -1,18 +1,22 @@
 package dev.klepto.unreflect;
 
 import dev.klepto.unreflect.property.Accessible;
-import dev.klepto.unreflect.reflection.ReflectionClassAccess;
+import dev.klepto.unreflect.reflection.*;
 import lombok.SneakyThrows;
 import lombok.val;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 /**
  * Unreflect is small but very powerful alternative to java reflection API. Provides easy to use type, class, field,
  * method and constructor lookup and access. Enables high-performance reflection by using code generation. Bypasses all
  * JVM security checks to enable access to any modules and members for both reflection based and bytecode generated API.
- * Reflection API is accessible by {@link Unreflect#reflect(Class)}.
- * Code-generated API is accessible by {@link Unreflect#unreflect(Class)}.
- * Library utilizes JDK's internal API and is not guaranteed to work with different versions of JDK, this library should
- * not be used in production.
+ * Reflection API is accessible by {@link Unreflect#reflect(Class)}. Code-generated API is accessible by
+ * {@link Unreflect#unreflect(Class)}. Library utilizes JDK's internal API and is not guaranteed to work with different
+ * versions of JDK, this library should not be used in production.
  *
  * @author <a href="http://github.com/klepto">Augustinas R.</a>
  * @see <a href="https://github.com/klepto/unreflect">GitHub Repository</a>
@@ -36,8 +40,8 @@ public class Unreflect {
     /**
      * Creates {@link ClassAccess} for a given class which generates bytecode for accessing its members. Access to any
      * fields, methods or constructors will automatically generate code for direct access to all members. Bytecode
-     * generation is very costly and should be only used during initialization in order to get performance benefits
-     * it provides over regular reflection. If you want bytecode to be only generated for specific members, use
+     * generation is very costly and should be only used during initialization in order to get performance benefits it
+     * provides over regular reflection. If you want bytecode to be only generated for specific members, use
      * {@link Unreflect#reflect(Object)} for lookup and then call {@link Accessible#unreflect()}.
      *
      * @param targetClass the target class
@@ -88,6 +92,49 @@ public class Unreflect {
      */
     public static <T> ClassAccess<T> reflect(Class<T> targetClass) {
         return new ReflectionClassAccess<>(targetClass, null);
+    }
+
+    /**
+     * Creates {@link ConstructorAccess} for a given reflection {@link Constructor}.
+     *
+     * @param targetConstructor the target constructor
+     * @return a constructor accessor pointing to the specified constructor
+     */
+    public static <T> ConstructorAccess<T> reflect(Constructor<T> targetConstructor) {
+        val classAccess = reflect(targetConstructor.getDeclaringClass());
+        return new ReflectionConstructorAccess<>(classAccess, targetConstructor);
+    }
+
+    /**
+     * Creates {@link MethodAccess} for a given reflection {@link Method}.
+     *
+     * @param targetMethod the target method
+     * @return a method accessor pointing to the specified method
+     */
+    public static MethodAccess reflect(Method targetMethod) {
+        val classAccess = reflect(targetMethod.getDeclaringClass());
+        return new ReflectionMethodAccess(classAccess, targetMethod, null);
+    }
+
+    /**
+     * Creates {@link FieldAccess} for a given reflection {@link Field}.
+     *
+     * @param targetField the target field
+     * @return a field accessor pointing to the specified field
+     */
+    public static FieldAccess reflect(Field targetField) {
+        val classAccess = reflect(targetField.getDeclaringClass());
+        return new ReflectionFieldAccess(classAccess, targetField, null);
+    }
+
+    /**
+     * Creates {@link ParameterAccess} for a given reflection {@link Parameter}.
+     *
+     * @param targetParameter the target parameter
+     * @return a parameter accessor pointing to the specified parameter
+     */
+    public static ParameterAccess reflect(Parameter targetParameter) {
+        return new ReflectionParameterAccess(null, targetParameter);
     }
 
 }
